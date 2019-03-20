4e94dc34da2451e148aea8647d03a190ff954fd7
Wreos <lozhkovoi@gmail.com>
2019-03-20 23:29:11 +0700

    added validation + улучшен роутин

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
 from app import app
 from app.forms import LoginForm
 
@@ -23,7 +23,11 @@ def index():
     ]
     return render_template('index.html', title='Домашняя',user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
     form=LoginForm()
-    return render_template('login.html',title='Авторизация', form=form)
+    if form.validate_on_submit():
+
+        flash('Login requested for user {}, password={} remember_me={}'.format(form.username.data, form.password.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',title='Sign in', form=form)
